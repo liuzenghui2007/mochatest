77e8011075f991bfaaa7c757a49e2f44acd39c11
zenghui <240164005@qq.com>
2019-12-09 18:41:03 +0800

    异步读文件

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
-module.exports = function (...rest) {
-    var sum = 0;
-    for (let n of rest) {
-        sum += n;
-    }
-    return sum;
+const fs = require('mz/fs');
+
+// a simple async function:
+module.exports = async () => {
+    let expression = await fs.readFile('./data.txt', 'utf-8');
+    let fn = new Function('return ' + expression);
+    let r = fn();
+    console.log(`Calculate: ${expression} = ${r}`);
+    return r;
 };
--- test/hello-test.js
+++ test/hello-test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sum = require('../hello');
+const fs = require('fs')
 
 describe('#hello.js', () => {
     describe('#sum()', () => {
@@ -19,20 +20,20 @@ describe('#hello.js', () => {
             console.log('  afterEach.');
         });
 
-        it('sum() should return 0', () => {
-            assert.strictEqual(sum(), 0);
+        // 可以测试同步函数，传入无参数函数即可
+        it('test sync function', function () {
+            // TODO:
+            assert(true);
         });
-
-        it('sum(1) should return 1', () => {
-            assert.strictEqual(sum(1), 1);
-        });
-
-        it('sum(1, 2) should return 3', () => {
-            assert.strictEqual(sum(1, 2), 3);
-        });
-
-        it('sum(1, 2, 3) should return 6', () => {
-            assert.strictEqual(sum(1, 2, 3), 6);
+        // 测试异步函数,读取文件
+        it('test async function', function (done) {
+            fs.readFile('./data.txt', function (err, data) {
+                if (err) {
+                    done(err);
+                } else {
+                    done();
+                }
+            });
         });
     });
 });
